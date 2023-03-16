76ac735e6598d5291e69edc699ec40d9c948d184
Thomas Bak <tombak98@gmail.com>
2023-03-15 23:19:19 -0400

    incorporated comments

--- components/About/BibleVerse.tsx
+++ components/About/BibleVerse.tsx
@@ -1,13 +1,18 @@
 import React from "react"
 import { Container } from "react-bootstrap"
 
-export default function BibleVerse(props:any) {
+interface Props {
+    verse: string
+    chapter: string
+}
+
+export default function BibleVerse({verse, chapter}:Props) {
     
     return (
         <Container id="about-bible-verse" className="bg-white shadow py-3 px-5 text-center d-flex flex-column align-items-center">
             <div className="verse-bar mb-4 mt-3 bg-primary"></div>
-            <p className="text-primary fs-5">{props.verse}</p>
-            <p className="text-primary fw-bold">{props.chapter}</p>
+            <p className="text-primary fs-5">{verse}</p>
+            <p className="text-primary fw-bold">{chapter}</p>
             <div className="verse-bar m-3 bg-primary"></div>
         </Container>
     )
