a42808e31293ec226d24d80ff1413aba5b054a10
wliu080 <simonliu911@gmail.com>
2023-04-18 11:35:35 +0900

    removed metadata json, switched to locale json

--- services/staticTopicLoader.ts
+++ services/staticTopicLoader.ts
@@ -4,42 +4,42 @@ import path from "path";
 const staticPageDirectory = path.join(process.cwd(), "pages/topics");
 
 function getTopicPageIds() {
-  const fileNames = fs
-    .readdirSync(staticPageDirectory, { withFileTypes: true })
-    .filter((fsDirent) => fsDirent.isDirectory())
-    .map((fsDirent) => fsDirent.name);
+    const fileNames = fs
+        .readdirSync(staticPageDirectory, { withFileTypes: true })
+        .filter((fsDirent) => fsDirent.isDirectory())
+        .map((fsDirent) => fsDirent.name);
 
-  // Returns an array that looks like this:
-  // the attr under parms HAS TO MATCH what is in [<this name>].tsx
-  // [
-  //   {
-  //     params: {
-  //       topicPage: 'ancestor-veneration'
-  //     }
-  //   },
-  //   {
-  //     params: {
-  //       topicPage: 'toolkit'
-  //     }
-  //   }
-  // ]
+    // Returns an array that looks like this:
+    // the attr under params HAS TO MATCH what is in [<this name>].tsx
+    // [
+    //   {
+    //     params: {
+    //       topicPage: 'ancestor-veneration'
+    //     }
+    //   },
+    //   {
+    //     params: {
+    //       topicPage: 'toolkit'
+    //     }
+    //   }
+    // ]
 
-  return fileNames.map((fileName) => {
-    return {
-      params: {
-        topicPage: fileName.replace(/\.md$/, ""),
-      },
-    };
-  });
+    return fileNames.map((fileName) => {
+        return {
+            params: {
+                topicPage: fileName.replace(/\.md$/, ""),
+            },
+        };
+    });
 }
 
 async function getTopicMetadata(topicPage: String) {
-  const fullPath = path.join(staticPageDirectory, `/${topicPage}/${topicPage}.json`);
-  const fileContents = fs.readFileSync(fullPath);
-  const metadata = JSON.parse(fileContents.toString());
-  
-  // Combine the data with the markdownPage
-  return metadata;
+    const fullPath = path.join(staticPageDirectory, `/${topicPage}/metadata-${topicPage}.json`);
+    const fileContents = fs.readFileSync(fullPath);
+    const metadata = JSON.parse(fileContents.toString());
+
+    // Combine the data with the markdownPage
+    return metadata;
 }
 
 export { getTopicPageIds, getTopicMetadata };
