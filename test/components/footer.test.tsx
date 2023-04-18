fc5fd66d6560692142fa1c6bb362bdae3650dd07
wliu080 <simonliu911@gmail.com>
2023-04-18 15:12:23 +0900

    Add workflow step to run tests on PR

--- test/components/footer.test.tsx
+++ test/components/footer.test.tsx
@@ -1,38 +1,63 @@
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
-import Footer from '../../components/footer';
-import { useRef } from 'react';
+import { render, screen } from "@testing-library/react";
+import Footer from "../../components/footer";
 
-describe('Footer', () => {
-    test('Renders the correct class name on the outer container', () => {
-        render(<Footer/>)
-        const footer = screen.getByTestId('footer')
-        expect(footer).toHaveClass('bg-primary')
-        expect(footer).toHaveClass('container-fluid')
-    })
+jest.mock("next/router", () => ({
+    useRouter: jest.fn(),
+}));
+jest.mock("react-i18next", () => ({
+    // this mock makes sure any components using the translate hook can use it without a warning being shown
+    useTranslation: () => {
+        return {
+            t: (str) => str,
+            i18n: {
+                changeLanguage: () => new Promise(() => {}),
+            },
+        };
+    },
+    initReactI18next: {
+        type: "3rdParty",
+        init: () => {},
+    },
+}));
 
-    test('Renders the correct text in the footer', () => {
-        render(<Footer/>)
-        const copyright = screen.getByTestId('copyright')
-        expect(copyright).toHaveTextContent('Copyright © 2021 Beneath the Surface. All rights reserved.')
-    })
+describe("Footer", () => {
+    beforeEach(() => {
+        const useRouter = jest.spyOn(require("next/router"), "useRouter");
+        useRouter.mockReturnValue({
+            push: () => {},
+        });
+    });
 
-    test('Certain components have a max-width (so that they are not too stretched in desktop view', () => {
-        render(<Footer/>)
-        const button = screen.getByTestId('signupButton')
-        const linksList = screen.getByTestId('linksList')
-        const maxWidth1 = getComputedStyle(button).maxWidth
-        const maxWidth2 = getComputedStyle(linksList).maxWidth
+    test("Renders the correct class name on the outer container", () => {
+        render(<Footer />);
+        const footer = screen.getByTestId("footer");
+        expect(footer).toHaveClass("bg-primary");
+        expect(footer).toHaveClass("container-fluid");
+    });
 
-        expect(maxWidth1).toBe('600px')
-        expect(maxWidth2).toBe('600px')
-    })
+    test("Renders the correct text in the footer", () => {
+        render(<Footer />);
+        const copyright = screen.getByTestId("copyright");
+        expect(copyright).toHaveTextContent("Copyright © 2021 Beneath the Surface. All rights reserved.");
+    });
 
-    test('Certain components shrink when in mobile view', () => {
-        global.innerWidth = 390
-        render(<Footer/>)
-        const button = screen.getByTestId('signupButton')
-        const width = button.offsetWidth
+    test("Certain components have a max-width (so that they are not too stretched in desktop view", () => {
+        render(<Footer />);
+        const button = screen.getByTestId("signupButton");
+        const linksList = screen.getByTestId("linksList");
+        const maxWidth1 = getComputedStyle(button).maxWidth;
+        const maxWidth2 = getComputedStyle(linksList).maxWidth;
 
-        expect(width).toBeLessThan(390)
-    })
-})
+        expect(maxWidth1).toBe("600px");
+        expect(maxWidth2).toBe("600px");
+    });
+
+    test("Certain components shrink when in mobile view", () => {
+        global.innerWidth = 390;
+        render(<Footer />);
+        const button = screen.getByTestId("signupButton");
+        const width = button.offsetWidth;
+
+        expect(width).toBeLessThan(390);
+    });
+});
