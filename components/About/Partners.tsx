861ee8e161686b571e30a092168cef91f719dcc6
wliu080 <simonliu911@gmail.com>
2023-04-25 10:40:45 +0900

    Partners unit tests

--- components/About/Partners.tsx
+++ components/About/Partners.tsx
@@ -1,20 +1,19 @@
-import React, { Suspense } from "react";
-import { Container, Image } from "react-bootstrap";
+import React from "react";
+import { Image } from "react-bootstrap";
 import { useTranslation } from "next-i18next";
 
-const Partners:React.FC = () => {
-    
+const Partners: React.FC = () => {
     const { t } = useTranslation("about");
 
     return (
-        <section id="partners-section" className="text-center">
+        <section data-testid="partners-section" id="partners-section" className="text-center">
             <p className="fs-6 fw-bold pt-5">{t("partners")}</p>
             <div className="d-flex align-items-center justify-content-center gap-3 pb-5">
                 <Image alt="omg logo" src="/omf.png"></Image>
                 <Image alt="pioneers logo" src="/pioneers_logo_full.png"></Image>
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default Partners
+export default Partners;
---
+++ test/components/About/Partners.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+import Partners from "../../../components/About/Partners";
+
+describe("Partners", () => {
+    test("Renders with the partner text and the two partner logos", () => {
+        render(<Partners />);
+        const partnersSection = screen.getByTestId("partners-section");
+
+        expect(partnersSection.tagName).toBe("SECTION");
+
+        expect(partnersSection.children[0].tagName).toBe("P");
+
+        const logoDiv = partnersSection.children[1];
+        expect(logoDiv.tagName).toBe("DIV");
+        expect(logoDiv.children.length).toBe(2);
+        expect(logoDiv.children[0].tagName).toBe("IMG");
+        expect(logoDiv.children[1].tagName).toBe("IMG");
+    });
+});
