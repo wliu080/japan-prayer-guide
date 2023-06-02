284e5d149116e06ce85af2793c80ac205d929f55
wliu080 <simonliu911@gmail.com>
2023-06-02 12:18:39 +0900

    Added index as part of key

--- pages/topics/[topicPage].tsx
+++ pages/topics/[topicPage].tsx
@@ -1,177 +1,177 @@
-import Head from "next/head";
-import { getTopicPageIds } from "../../services/staticTopicLoader";
-import { GetStaticProps, GetStaticPaths } from "next";
-import { Container } from "react-bootstrap";
-import { ToggleHeader } from "../../components/toggleHeader";
-import TopicDownloadables from "../../components/topic/TopicDownloadables";
-import RelatedContent from "../../components/topic/RelatedContent";
-import Footer from "../../components/footer";
-import { useTranslation } from "next-i18next";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import Feedback from "../../components/topic/Feedback";
-import ImageGroup from "../../components/topic/ImageGroup";
-import { TopicNav } from "../../components/topic/TopicNav";
-import React from "react";
-import PrayerPoints from "../../components/common/PrayerPoints";
+import Head from "next/head"
+import { getTopicPageIds } from "../../services/staticTopicLoader"
+import { GetStaticProps, GetStaticPaths } from "next"
+import { Container } from "react-bootstrap"
+import { ToggleHeader } from "../../components/toggleHeader"
+import TopicDownloadables from "../../components/topic/TopicDownloadables"
+import RelatedContent from "../../components/topic/RelatedContent"
+import Footer from "../../components/footer"
+import { useTranslation } from "next-i18next"
+import { serverSideTranslations } from "next-i18next/serverSideTranslations"
+import Feedback from "../../components/topic/Feedback"
+import ImageGroup from "../../components/topic/ImageGroup"
+import { TopicNav } from "../../components/topic/TopicNav"
+import React from "react"
+import PrayerPoints from "../../components/common/PrayerPoints"
 
 export const getStaticPaths: GetStaticPaths = async () => {
-    const paths = getTopicPageIds();
+  const paths = getTopicPageIds()
 
-    return {
-        paths,
-        fallback: false,
-    };
-};
+  return {
+    paths,
+    fallback: false,
+  }
+}
 
 export const getStaticProps: GetStaticProps = async ({ params, locale }: any) => {
-    if (!params) {
-        return {
-            props: {},
-        };
-    }
-
-    // const topicId: string = params.topicPage
-    const localeRef: string = "topics/" + params.topicPage;
-
+  if (!params) {
     return {
-        props: {
-            localeRef,
-            ...(await serverSideTranslations(locale, ["common", localeRef])),
-        },
-    };
-};
+      props: {},
+    }
+  }
+
+  // const topicId: string = params.topicPage
+  const localeRef: string = "topics/" + params.topicPage
+
+  return {
+    props: {
+      localeRef,
+      ...(await serverSideTranslations(locale, ["common", localeRef])),
+    },
+  }
+}
 
 export default function TopicPage({ localeRef }: { localeRef: string }) {
-    const { t } = useTranslation(localeRef);
+  const { t } = useTranslation(localeRef)
 
-    // Objects holding translations
-    const summaryPoints: string[] = t("summary.content", { returnObjects: true });
-    const summaryTitle: string = t("summary.title");
+  // Objects holding translations
+  const summaryPoints: string[] = t("summary.content", { returnObjects: true })
+  const summaryTitle: string = t("summary.title")
 
-    const bodyContent1: string[] = t("mainBody.content1", { returnObjects: true });
-    const bodyContent2: string[] = t("mainBody.content2", { returnObjects: true });
-    const bodyContent3: string[] = t("mainBody.content3", { returnObjects: true });
+  const bodyContent1: string[] = t("mainBody.content1", { returnObjects: true })
+  const bodyContent2: string[] = t("mainBody.content2", { returnObjects: true })
+  const bodyContent3: string[] = t("mainBody.content3", { returnObjects: true })
 
-    const quoteContent: string = t("quote.content");
-    const quoteSource: string = t("quote.source");
+  const quoteContent: string = t("quote.content")
+  const quoteSource: string = t("quote.source")
 
-    const dykTitle: string = t("didYouKnow.title");
-    const dykContent: string = t("didYouKnow.content");
+  const dykTitle: string = t("didYouKnow.title")
+  const dykContent: string = t("didYouKnow.content")
 
-    const prayerTitle: string = t("prayerPoints.title");
-    const prayerSub: string = t("prayerPoints.subtitle");
+  const prayerTitle: string = t("prayerPoints.title")
+  const prayerSub: string = t("prayerPoints.subtitle")
 
-    const feedbackTitle: string = t("feedback.title");
-    const feedbackButton: string = t("feedback.button");
+  const feedbackTitle: string = t("feedback.title")
+  const feedbackButton: string = t("feedback.button")
 
-    const downloadsTitle: string = t("downloads.title");
-    const downloadsLabels: string[] = t("downloads.labels", { returnObjects: true });
-    const downloadsLinks: string[] = t("downloads.links", { returnObjects: true });
-    const downloadsHeaders: string[] = t("downloads.headers", { returnObjects: true });
+  const downloadsTitle: string = t("downloads.title")
+  const downloadsLabels: string[] = t("downloads.labels", { returnObjects: true })
+  const downloadsLinks: string[] = t("downloads.links", { returnObjects: true })
+  const downloadsHeaders: string[] = t("downloads.headers", { returnObjects: true })
 
-    const relatedTitle: string = t("related.title");
-    const relatedLabels: string[] = t("related.labels", { returnObjects: true });
-    const relatedLinks: string[] = t("related.links", { returnObjects: true });
+  const relatedTitle: string = t("related.title")
+  const relatedLabels: string[] = t("related.labels", { returnObjects: true })
+  const relatedLinks: string[] = t("related.links", { returnObjects: true })
 
-    const navLabels: string[] = t("topicNav.labels", { returnObjects: true });
+  const navLabels: string[] = t("topicNav.labels", { returnObjects: true })
 
-    const pageTitle: string = t("title");
+  const pageTitle: string = t("title")
 
-    // States
-    const [selected, setSelected] = React.useState<string>("About");
+  // States
+  const [selected, setSelected] = React.useState<string>("About")
 
-    return (
-        <>
-            <Head>
-                <title>{pageTitle}</title>
-                <meta name="description" content="Japan prayer guide" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <main>
-                {/* Header Component */}
-                <ToggleHeader />
+  return (
+    <>
+      <Head>
+        <title>{pageTitle}</title>
+        <meta name="description" content="Japan prayer guide" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        {/* Header Component */}
+        <ToggleHeader />
 
-                {/* Component to Hold image, as well as title and Prayer Summary */}
-                <div id="placeholder-image" className="w-100 mx-0 px-0 position-relative" style={{ height: "700px" }}>
-                    temporary image placeholder
-                    <Container className="d-flex flex-column align-items-start justify-content-end h-100 py-5">
-                        <h1 className="fs-1 text-white px-3">{t("title")}</h1>
-                        <PrayerPoints prayerPoints={summaryPoints} title={summaryTitle} />
-                    </Container>
-                </div>
+        {/* Component to Hold image, as well as title and Prayer Summary */}
+        <div id="placeholder-image" className="w-100 mx-0 px-0 position-relative" style={{ height: "700px" }}>
+          temporary image placeholder
+          <Container className="d-flex flex-column align-items-start justify-content-end h-100 py-5">
+            <h1 className="fs-1 text-white px-3">{t("title")}</h1>
+            <PrayerPoints prayerPoints={summaryPoints} title={summaryTitle} />
+          </Container>
+        </div>
 
-                {/* Topic Nav Component */}
-                <TopicNav selected={selected} setSelected={setSelected} labels={navLabels} />
+        {/* Topic Nav Component */}
+        <TopicNav selected={selected} setSelected={setSelected} labels={navLabels} />
 
-                {/* Video/Reel Placeholder */}
-                <Container className="py-5" id="topic-about">
-                    <div id="placeholder-image" className="w-100">
-                        Video Placeholder
-                    </div>
-                </Container>
+        {/* Video/Reel Placeholder */}
+        <Container className="py-5" id="topic-about">
+          <div id="placeholder-image" className="w-100">
+            Video Placeholder
+          </div>
+        </Container>
 
-                {/* Placeholder text */}
-                <Container className="py-5">
-                    {bodyContent1.map((text) => (
-                        <p key={text}>{text}</p>
-                    ))}
-                </Container>
+        {/* Placeholder text */}
+        <Container className="py-5">
+          {bodyContent1.map((text: string, idx: number) => (
+            <p key={idx + text}>{text}</p>
+          ))}
+        </Container>
 
-                {/* Image Grid */}
-                <ImageGroup />
+        {/* Image Grid */}
+        <ImageGroup />
 
-                {/* Placeholder text */}
-                <Container className="py-5">
-                    {bodyContent2.map((text) => (
-                        <p key={text}>{text}</p>
-                    ))}
-                </Container>
+        {/* Placeholder text */}
+        <Container className="py-5">
+          {bodyContent2.map((text: string, idx: number) => (
+            <p key={idx + text}>{text}</p>
+          ))}
+        </Container>
 
-                {/* Infographics Placeholder */}
-                <Container>
-                    <div id="placeholder-image" className="w-100">
-                        Infographics Placeholder
-                    </div>
-                </Container>
+        {/* Infographics Placeholder */}
+        <Container>
+          <div id="placeholder-image" className="w-100">
+            Infographics Placeholder
+          </div>
+        </Container>
 
-                {/* Placeholder for a quote */}
-                <Container className="d-flex flex-column justify-content-center align-items-center py-4 my-4">
-                    <h1 className="text-secondary fs-1 text-center">{quoteContent}</h1>
-                    <h2 className="text-secondary fs-6 text-center">{quoteSource}</h2>
-                </Container>
+        {/* Placeholder for a quote */}
+        <Container className="d-flex flex-column justify-content-center align-items-center py-4 my-4">
+          <h1 className="text-secondary fs-1 text-center">{quoteContent}</h1>
+          <h2 className="text-secondary fs-6 text-center">{quoteSource}</h2>
+        </Container>
 
-                {/* Placeholder Image */}
-                <div id="placeholder-image" className="w-100 my-5">
-                    Placeholder
-                </div>
+        {/* Placeholder Image */}
+        <div id="placeholder-image" className="w-100 my-5">
+          Placeholder
+        </div>
 
-                {/* Placeholder text */}
-                <Container className="py-5">
-                    {bodyContent3.map((text) => (
-                        <p key={text}>{text}</p>
-                    ))}
-                </Container>
+        {/* Placeholder text */}
+        <Container className="py-5">
+          {bodyContent3.map((text: string, idx: number) => (
+            <p key={idx + text}>{text}</p>
+          ))}
+        </Container>
 
-                {/* Prayer Points */}
-                <PrayerPoints prayerPoints={summaryPoints} title={prayerTitle} showImg={true} showSubtitle={true} />
-                <br />
+        {/* Prayer Points */}
+        <PrayerPoints prayerPoints={summaryPoints} title={prayerTitle} showImg={true} showSubtitle={true} />
+        <br />
 
-                {/* Give us Feedback */}
-                <Feedback title={feedbackTitle} button={feedbackButton} />
+        {/* Give us Feedback */}
+        <Feedback title={feedbackTitle} button={feedbackButton} />
 
-                {/* Downloads and Related */}
-                <TopicDownloadables
-                    links={downloadsLinks}
-                    labels={downloadsLabels}
-                    title={downloadsTitle}
-                    headers={downloadsHeaders}
-                />
-                <RelatedContent topics={relatedLabels} links={relatedLinks} title={relatedTitle} />
+        {/* Downloads and Related */}
+        <TopicDownloadables
+          links={downloadsLinks}
+          labels={downloadsLabels}
+          title={downloadsTitle}
+          headers={downloadsHeaders}
+        />
+        <RelatedContent topics={relatedLabels} links={relatedLinks} title={relatedTitle} />
 
-                {/* Footer */}
-                <Footer />
-            </main>
-        </>
-    );
+        {/* Footer */}
+        <Footer />
+      </main>
+    </>
+  )
 }
