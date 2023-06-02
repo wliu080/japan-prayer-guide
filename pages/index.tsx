64e48e216a4b6a6b34dc17943f8b4a0d736803aa
wliu080 <simonliu911@gmail.com>
2023-06-02 11:57:04 +0900

    Switch to the new featured topic component

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,75 +5,82 @@ import { useTranslation } from "next-i18next"
 import { serverSideTranslations } from "next-i18next/serverSideTranslations"
 import { getSchedule, getFeaturedTopic } from "../services/featuredTopicSelector"
 import Footer from "../components/footer"
-import TopicPrayerPoints from "../components/topic/TopicPrayerPoints"
+import FeaturedTopic from "../components/landing/FeaturedTopic"
 
 export const getStaticProps = async ({ locale }: { locale: string }) => {
-    const featuredTopicRef: string = "topics/" + getFeaturedTopic(getSchedule())
+  const featuredTopicRef: string = "topics/" + getFeaturedTopic(getSchedule())
 
-    return {
-        props: {
-            featuredTopicRef,
-            ...(await serverSideTranslations(locale, ["common", featuredTopicRef])),
-            // Will be passed to the page component as props
-        },
-    }
+  return {
+    props: {
+      featuredTopicRef,
+      ...(await serverSideTranslations(locale, ["common", "home", featuredTopicRef])),
+      // Will be passed to the page component as props
+    },
+  }
 }
 
 const Home = ({ featuredTopicRef }: { featuredTopicRef: string }) => {
-    const { t } = useTranslation("common")
-    const { t: featuredTranslation } = useTranslation(featuredTopicRef)
+  const { t } = useTranslation("common")
+  const { t: homePageTranslation } = useTranslation("home")
+  const { t: featuredTranslation } = useTranslation(featuredTopicRef)
 
-    const prayerPoints: string[] = featuredTranslation("summary.content", { returnObjects: true })
-    const prayerTitle: string = featuredTranslation("title")
-    const prayerSub: string = featuredTranslation("prayerPoints.subtitle")
+  const heroHeading: string = homePageTranslation("heroHeading")
+  const heroViewTopicsBtn: string = homePageTranslation("viewTopics")
+  const mainBlurb: string = homePageTranslation("mainBlurb")
+  const featuredTopicTitle: string = homePageTranslation("featuredTopicTitle")
+  const featuredTopicSubtitle: string = homePageTranslation("featuredTopicSubtitle")
 
-    return (
-        <div>
-            <Head>
-                <title>{t("title")}</title>
-                <meta name="description" content="Japan prayer guide" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <main>
-                <ToggleHeader />
+  const prayerPoints: string[] = featuredTranslation("summary.content", { returnObjects: true })
+  const prayerTitle: string = featuredTranslation("title")
+  const prayerSub: string = featuredTranslation("prayerPoints.subtitle")
 
-                {/* Hero banner? section */}
-                <Container fluid id="heroBannerSection" className="w-100 bg-secondary" style={{ height: "400px" }}>
-                    temporary image placeholder
-                    <Container className="d-flex flex-column align-items-center justify-content-center h-100 py-4">
-                        <h1 className="text-white">{t("home.heroHeading")}</h1>
-                        <Button href="/about" className="text-white">
-                            {t("home.viewTopics")}
-                        </Button>
-                    </Container>
-                </Container>
+  return (
+    <div>
+      <Head>
+        <title>{t("title")}</title>
+        <meta name="description" content="Japan prayer guide" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <ToggleHeader />
 
-                <Container fluid id="mainBlurbSection" className="bg-white p-5 w-75">
-                    <p className="px-3">{t("home.mainBlurb")}</p>
-                </Container>
+        {/* Hero banner? section */}
+        <Container fluid id="heroBannerSection" className="w-100 bg-secondary" style={{ height: "400px" }}>
+          temporary image placeholder
+          <Container className="d-flex flex-column align-items-center justify-content-center h-100 py-4">
+            <h1 className="text-white">{heroHeading}</h1>
+            <Button href="/about" className="text-white">
+              {heroViewTopicsBtn}
+            </Button>
+          </Container>
+        </Container>
 
-                {/* Why Japan component */}
+        <Container fluid id="mainBlurbSection" className="bg-white p-5 w-75">
+          <p className="px-3">{mainBlurb}</p>
+        </Container>
 
-                {/* Featured topic component - has to potentially be pulled from a schedule that Sarah can edit */}
-                <Container fluid id="featuredTopicSection" className="py-5 bg-warning">
-                    <Container className="d-flex flex-column align-items-center justify-content-center">
-                        <h3 className="text-white">{t("home.featuredTopicTitle")}</h3>
-                        <h4 className="text-white">{t("home.featuredTopicSubtitle")}</h4>
-                    </Container>
-                    <TopicPrayerPoints prayerPoints={prayerPoints} title={prayerTitle} subtitle={prayerSub} />
-                </Container>
+        {/* Why Japan component */}
 
-                {/* Downloads snippet */}
+        {/* Featured topic */}
+        <Container fluid id="featuredTopicSection" className="py-5 bg-warning">
+          <Container className="d-flex flex-column align-items-center justify-content-center text-white">
+            <h3>{featuredTopicTitle}</h3>
+            <h4>{featuredTopicSubtitle}</h4>
+          </Container>
+          <FeaturedTopic title={prayerTitle} prayerPoints={prayerPoints} subtitle={prayerSub} />
+        </Container>
 
-                {/* Purchase snippet */}
+        {/* Downloads snippet */}
 
-                {/* About snippet */}
+        {/* Purchase snippet */}
 
-                <Footer />
-            </main>
-        </div>
-    )
+        {/* About snippet */}
+
+        <Footer />
+      </main>
+    </div>
+  )
 }
 
 export default Home
