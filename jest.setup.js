e4e195185e0a5dd5bfbcfd4f70e7375068135117
Thomas Bak <tombak98@gmail.com>
2023-07-10 19:15:24 -0400

    fixed weird thing with react-slick, needed a matchMedia?

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,11 @@
 import '@testing-library/jest-dom/extend-expect'
+
+window.matchMedia =
+  window.matchMedia ||
+  function() {
+    return {
+      matches: false,
+      addListener: function() {},
+      removeListener: function() {}
+    };
+  };
