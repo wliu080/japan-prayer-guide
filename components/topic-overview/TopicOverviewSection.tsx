112a85259fe4d9a8f8084a458fe670d7265c30f6
Thomas Bak <tombak98@gmail.com>
2023-08-16 20:51:12 -0400

    improved look of topic overview nav page

--- components/topic-overview/TopicOverviewSection.tsx
+++ components/topic-overview/TopicOverviewSection.tsx
@@ -13,24 +13,25 @@ export const TopicOverviewSection = ({title, links, labels, section}:TopicOvervi
     return (
         <>
             <section>
-                <Container id={section} className="no-max-container mt-4 mt-md-5">
-                    <h1 className="topic-nav-section-title text-primary pt-4 px-0 px-md-2 mb-0">{title}</h1>
+                <Container id={section} className="no-max-container mt-4 mt-md-4">
+                    <h1 className="topic-nav-section-title text-primary pt-4 px-1 px-md-2 mb-0">{title}</h1>
                 </Container>
-                <Container className="bottom-grey-border px-4 pt-1 no-max-container pb-4">
-                    <Row xl={3} lg={3} md={2} sm={2} xs={2}>
+                <Container className="bottom-grey-border px-4 pt-1 no-max-container pb-5">
+                    <Row xl={3} lg={3} md={2} sm={2} xs={2} className="d-flex">
                         {labels.map((element, idx) => {
                             // Special if statement for conference, the 'else' will be removed for final
                             if ((idx === 0 && section === "culture") || (idx === 2 && section === "church")) {
                                 return (
-                                    <Col key={element + idx} className="d-flex justify-content-center">
+                                    <Col key={element + idx} className="d-flex justify-content-center px-0 px-sm-1 px-md-2">
                                         <Link href={`/topics/${links[idx].toLowerCase()}`} className="text-decoration-none">
                                             <Card className="topic-nav-card mx-1 my-3">
                                                 <Image
                                                     src={`/photos/topic-nav/${section}/${links[idx]}.png`}
                                                     alt={""}
+                                                    style={{aspectRatio: 1.772, objectFit: 'cover', width: '100%'}}
                                                 />
-                                                <Card.Body style={{height: 'fit-content'}} className="d-flex align-items-center topic-nav-card-title">
-                                                    <p className="m-0">{element}</p>
+                                                <Card.Body className="d-flex topic-nav-card-title p-0">
+                                                    <p className="my-0">{element}</p>
                                                 </Card.Body>
                                             </Card>
                                         </Link>
@@ -38,19 +39,19 @@ export const TopicOverviewSection = ({title, links, labels, section}:TopicOvervi
                                 )
                             } else {
                                 return (
-                                    <Col key={element + idx} className="d-flex justify-content-center">
+                                    <Col key={element + idx} className="d-flex justify-content-center px-0 px-sm-1 px-md-2">
                                         <Link href={''} className="text-decoration-none position-relative">
                                             <Card className="topic-nav-card mx-1 my-3">
-                                                <div className="position-relative" style={{width: 'fit-content', height: 'fit-content'}}>
+                                                <div className="position-relative" style={{width: 'fit-content', height: 'fit-content', borderRadius: '8px 8px 0px 0px'}}>
                                                     <Image
                                                         src={`/photos/topic-nav/${section}/placeholder.png`}
                                                         alt={""}
-                                                        style={{aspectRatio: 1.772, objectFit: 'cover', width: '100%'}}
+                                                        style={{aspectRatio: 1.772, objectFit: 'cover', width: '100%', borderRadius: '4px 4px 0px 4px'}}
                                                     />
-                                                    <div style={{zIndex: 2, top: 0, cursor: 'not-allowed', backgroundColor: '#727C96'}} className="position-absolute w-100 h-100 opacity-75"></div>
+                                                    <div style={{zIndex: 2, top: 0, cursor: 'not-allowed', backgroundColor: '#727C96', borderRadius: '10px 10px 0px 0px'}} className="position-absolute w-100 h-100 opacity-75"></div>
                                                 </div>
-                                                <Card.Body className="d-flex align-items-center topic-nav-card-title" style={{cursor: 'not-allowed'}}>
-                                                    <p className="m-0">{element}</p>
+                                                <Card.Body className="d-flex topic-nav-card-title p-0" style={{cursor: 'not-allowed'}}>
+                                                    <p className="m-0 text-grey-6">{element}</p>
                                                 </Card.Body>
                                             </Card>
                                         </Link>
