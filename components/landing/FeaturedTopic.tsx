312ac0ce88a3e4975e8ab822de47a63ab4559378
wliu080 <simonliu911@gmail.com>
2023-08-18 11:03:03 +0900

    formatting fixes + fix for test-on-pr

--- components/landing/FeaturedTopic.tsx
+++ components/landing/FeaturedTopic.tsx
@@ -12,14 +12,13 @@ interface Props {
 }
 
 export default function FeaturedTopic({ title, prayerPoints, featuredImg }: Props) {
-
   const { t } = useTranslation("common")
 
   const subtitle: string = t("prayerSummary.subtitle")
   const read: string = t("prayerSummary.read")
   const view: string = t("prayerSummary.view")
 
-  const redirect = (location:string) => {
+  const redirect = (location: string) => {
     window.location.replace(location)
   }
 
@@ -27,7 +26,7 @@ export default function FeaturedTopic({ title, prayerPoints, featuredImg }: Prop
     <Container className="d-flex justify-content-center">
       <Card className="my-4 shadow d-lg-flex flex-lg-row">
         {/* temporary height for image, before we finalize images */}
-        <Card.Img variant="top" src={featuredImg} className="home-feature-img"/>
+        <Card.Img variant="top" src={featuredImg} className="home-feature-img" />
         <Card.Body className="p-4" style={{ maxWidth: "600px" }}>
           <Card.Text className="featuredTopicTitle px-2 pb-2 fs-2 fw-bold border-bottom border-grey">{title}</Card.Text>
           <Card.Text className="featuredTopicTitle d-flex align-items-center gap-2 text-secondary-5 fw-bold fs-3">
@@ -49,7 +48,7 @@ export default function FeaturedTopic({ title, prayerPoints, featuredImg }: Prop
           <Card.Text
             className="mt-3 w-100 mx-auto text-center text-decoration-underline text-secondary-5 fw-bold"
             style={{ cursor: "pointer" }}
-            onClick={() => redirect('topics/all')}
+            onClick={() => redirect("topics/all")}
           >
             {view}
           </Card.Text>
