6d4c8c0553b03d131ceb3483ef1d499a742cf448
jayjhan8 <jayh@qualtrics.com>
2023-08-21 11:48:51 -0700

    Image pagination and main content on booklet

--- pages/booklet.tsx
+++ pages/booklet.tsx
@@ -4,13 +4,16 @@ import Link from "next/link"
 import React from "react"
 import { ToggleHeader } from "../components/toggleHeader"
 import { PurchaseButtons } from "../components/purchase/PurchaseButtons"
-import { Container } from "react-bootstrap"
+import { Container, Row, Col } from "react-bootstrap"
+import BootstrapImage from "react-bootstrap/Image"
 import { useTranslation } from "next-i18next"
 import { serverSideTranslations } from "next-i18next/serverSideTranslations"
-import BookImage from "../public/front-cover-sm-424x600 1.svg"
 import SampleBookPageImage from "../public/sample book page.svg"
+import SampleBookPageImage2 from "../public/photos/booklet/Book - Slider 1 JP 3.png"
+import SampleBookPageImage3 from "../public/photos/booklet/BOOK_GIF_JP 4.png"
 import TranslationIcon from "../public/translationIcon.svg"
 import Footer from "../components/footer"
+import ImagePagination from "../components/image-pagination/ImagePagination"
 
 export async function getStaticProps({ locale }: any) {
   return {
@@ -35,30 +38,44 @@ const Booklet: React.FC = () => {
       </Head>
       <ToggleHeader />
       <main id="purchase">
-        <div className="book-description">
-          <div className="book-image">
-            <Image className="book-front-cover" src={BookImage} alt={t("bookImageAlt")} />
-          </div>
-          <div className="book-text">
-            <h1>{t("heading")}</h1>
-            <p className="book-subtext">{t("subtext")}</p>
-            <p className="book-subtitle">{t("subtitle")}</p>
-            <h3>{t("purchase")}: </h3>
-            <div className="purchase-links">
-              <PurchaseButtons />
-              <Link href="https://amazon.com" target="_blank">
-                {t("order")}
-              </Link>
-            </div>
-          </div>
+        <div className="w-100 book-description position-relative">
+          <Container>
+            <Row md={1} lg={2}>
+              <Col>
+                <div className="book-image">
+                  <BootstrapImage
+                    className="book-front-cover"
+                    src="/photos/booklet/Book - Cover EN 2.png"
+                    alt={t("bookImageAlt")!}
+                  />
+                </div>
+              </Col>
+              <Col className="d-flex align-items-center">
+                <div className="book-text">
+                  <h1 className="display-1"><i>{t("heading")}</i></h1>
+                  <p className="book-subtext"><i>{t("subtext")}</i></p>
+                  <p className="book-subtitle">{t("subtitle")}</p>
+                </div>
+              </Col>
+            </Row>
+          </Container>
         </div>
         <Container id="prayer-sample" className="py-5">
-          <div id="prayer-landing-image" className="w-100 p-3">
-            temporary image placeholder
-          </div>
           <Container className="page-container py-3">
-            <Image src={SampleBookPageImage} alt={t("samplePageImageAlt")} />
+            <ImagePagination
+              pages={[
+                { src: SampleBookPageImage, text: t('samplePageImageAlt') },
+                { src: SampleBookPageImage2, text: "" },
+                { src: "", text: "" },
+              ]}
+            />
           </Container>
+        </Container>
+        <Container>
+          <BootstrapImage
+            className="w-100 px-10"
+            src="/photos/booklet/BOOK_GIF_JP 4.png"
+          />
         </Container>
         <section className="purchase-section">
           <Container className="text-center">
