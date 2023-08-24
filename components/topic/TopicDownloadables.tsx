826ebd605587ad568a80ad07d8527e2656d7c724
wliu080 <simonliu911@gmail.com>
2023-08-24 13:53:10 +0900

    ensure locale is provided with links

--- components/topic/TopicDownloadables.tsx
+++ components/topic/TopicDownloadables.tsx
@@ -4,6 +4,7 @@ import Button from "react-bootstrap/Button"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 import Link from "next/link"
+import { useTranslation } from "react-i18next"
 
 interface downloadProps {
   links: string[]
@@ -13,20 +14,21 @@ interface downloadProps {
 }
 
 export default function TopicDownloadables({ links, title, labels, headers }: downloadProps) {
+  const { i18n } = useTranslation("common")
   return (
     <Container data-testid={"topic-downloadables-container"} className="d-flex flex-column my-5" id="topic-downloads">
       <Container className="d-flex flex-row justify-content-between align-items-center">
         <h1 data-testid={"topic-downloadables-title"} className="text-primary my-4 fs-1">
           {title}
         </h1>
-        <a href={"/downloads"} className="text-secondary d-none d-md-block">
+        <Link href={"/downloads"} className="text-secondary d-none d-md-block" locale={i18n.language}>
           {headers[0]}
-        </a>
+        </Link>
       </Container>
       <Row xl={4} lg={2} className="g-3" data-testid={"topic-downloadables-links"}>
         {labels.map((label, idx) => (
           <Col key={idx + label}>
-            <Link href={links[idx]} className="text-decoration-none">
+            <Link href={links[idx]} className="text-decoration-none" locale={i18n.language}>
               <Card className="shadow-sm border-0 rounded" style={{ backgroundColor: "#E2E2E2", minHeight: "125px" }}>
                 <Card.Body className="d-flex align-items-center justify-content-center">
                   <p className="text-center fw-bold fs-4 my-0">{label}</p>
@@ -36,14 +38,14 @@ export default function TopicDownloadables({ links, title, labels, headers }: do
           </Col>
         ))}
       </Row>
-      <Link href={links[4]} className="align-self-center w-100" style={{ maxWidth: "500px" }}>
+      <Link href={links[4]} className="align-self-center w-100" style={{ maxWidth: "500px" }} locale={i18n.language}>
         <Button className="align-self-center w-100 mt-4 text-white border-secondary bg-secondary" variant="primary">
           {headers[1]}
         </Button>
       </Link>
-      <a href={"/downloads"} className="align-self-center my-3 text-secondary d-md-none">
+      <Link href={"/downloads"} className="align-self-center my-3 text-secondary d-md-none" locale={i18n.language}>
         {headers[0]}
-      </a>
+      </Link>
     </Container>
   )
 }
