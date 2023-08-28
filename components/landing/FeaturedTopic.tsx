175bf6393480e81a3cde0c1ac4310200e4b3d51b
Thomas Bak <tombak98@gmail.com>
2023-08-28 19:22:38 -0400

    comments incorporateed for home page

--- components/landing/FeaturedTopic.tsx
+++ components/landing/FeaturedTopic.tsx
@@ -24,13 +24,9 @@ export default function FeaturedTopic({ title, prayerPoints, featuredImg }: Prop
   }
 
   return (
-    <Container className="d-flex justify-content-center">
-<<<<<<< HEAD
-      <Card className="my-4 shadow d-lg-flex flex-lg-row">
-=======
+    <Container className="d-flex justify-content-center featured-topic-container">
       <Card className="my-4 shadow d-md-flex flex-md-row">
         {/* temporary height for image, before we finalize images */}
->>>>>>> 8f3363b (update sizes according to design library)
         <Card.Img variant="top" src={featuredImg} className="home-feature-img" />
         <Card.Body className="p-4" style={{ maxWidth: "600px" }}>
           <Card.Text className="featuredTopicTitle px-2 pb-2 fs-2 border-bottom border-grey">{title}</Card.Text>
@@ -44,7 +40,7 @@ export default function FeaturedTopic({ title, prayerPoints, featuredImg }: Prop
           </Card.Text>
           <ul className="bullet-points">
             {prayerPoints.map((point: string, idx: number) => (
-              <li key={idx + point} className="my-2">
+              <li key={idx + point} className="my-2 bullet-point">
                 {point}
               </li>
             ))}
