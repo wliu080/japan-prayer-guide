e15d0ef6f3b2458cc8666ef58ba12fa2cd0ba6f3
Thomas Bak <tombak98@gmail.com>
2023-08-31 21:28:33 -0400

    about page clean up and copy

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,7 +2,7 @@ import Head from "next/head"
 import React from "react"
 import { ToggleHeader } from "../components/toggleHeader"
 import { Container, Image } from "react-bootstrap"
-import { useTranslation } from "next-i18next"
+import { useTranslation, Trans } from "next-i18next"
 import { serverSideTranslations } from "next-i18next/serverSideTranslations"
 import Footer from "../components/footer"
 import MissionVision from "../components/About/MissionVision"
@@ -58,33 +58,36 @@ const About: React.FC = () => {
         </div>
 
         {/* Growing interest section */}
-        <Container id="who-are-we" className="w-100 d-flex flex-column align-items-center">
+        <Container id="who-are-we" className="w-100 d-flex flex-column align-items-center px-3 px-md-4">
           <h1 className="text-primary about-h1-header text-center">{introTitle}</h1>
-          <Image className="mt-3" alt="about img 1" src="/photos/about/about_01.png"/>
-          <div className="w-100 mt-5"></div>
+          <Image className="mt-2 mt-md-4 about-main-image" alt="about img 1" src="/photos/about/about_01.png"/>
+          <div className="w-100 mt-4 mt-md-5"></div>
           {introBlurb.map((text, idx) =>
             <p key={idx + text} className="about-body-text">{text}</p>
           )}
         </Container>
 
         {/* Beneath section */}
-        <div className="bg-secondary-2 w-100 py-5 d-flex align-items-center flex-column">
-          <Container className="about-beneath-surface">
-            <h1 className="mb-5 text-primary about-h1-header text-center">{contextTitle}</h1>
-            <Container className="d-flex flex-column flex-md-row gap-3 about-images justify-content-between">
-              <Image alt="about img 2" src="/photos/about/about_01.png" className=""/>
-              <Image alt="about img 3" src="/photos/about/about_02.png" className=""/>
+        <div className="bg-secondary-2 w-100 pb-4 pb-md-5 pt-5 d-flex align-items-center flex-column px-3 px-md-4">
+          <Container className="about-beneath-surface px-0">
+            <h1 className="mb-3 mb-md-5 text-primary about-h1-header text-center fst-italic">{contextTitle}</h1>
+            <Container className="d-flex flex-column flex-md-row gap-3 gap-xl-4 about-images justify-content-center px-0">
+              <Image alt="about img 2" src="/photos/about/about_02.png" className="about-image"/>
+              <Image alt="about img 3" src="/photos/about/about_03.png" className="about-image"/>
             </Container>
           </Container>
-          <div className="mt-5"></div>
-          {contextBlurb.map((text, idx) =>
-            <p key={idx + text} className="about-body-text px-3">{text}</p>
-          )}
+          <div className="mt-4 mt-md-5"></div>
+            <p className="about-body-text px-0">
+                <Trans t={t} i18nKey='contextBlurb.0' components={{italic: <i />}}/>
+            </p>
+            <p className="about-body-text px-0">
+                <Trans t={t} i18nKey='contextBlurb.1' components={{italic: <i />}}/>
+            </p>
         </div>
 
         {/* Verse section */}
-        <div className="w-100 pb-3 pt-5 px-3 bg-grey-7">
-          <Container className="w-100 py-5 about-verse">
+        <div className="w-100 px-3 pt-5 pb-4 bg-grey-7">
+          <Container className="w-100 about-verse">
             <p className="text-white text-center fst-italic">{bibleContent}</p>
             <p className="text-white text-center about-passage-location mt-3">{biblePassageLocation}</p>
           </Container>
@@ -94,7 +97,7 @@ const About: React.FC = () => {
         <Container className="w-100 about-invite px-4">
           <div className="d-flex flex-column align-items-center border-grey-4 border-bottom">
             <h1 className="mt-5 about-h1-header text-black text-center">{inviteTitle}</h1>
-            <p className=" mt-4 mb-5 about-body-text">{inviteBlurb} <strong>{callToAction}</strong></p>
+            <p className="mt-3 mb-5 about-body-text">{inviteBlurb} <strong>{callToAction}</strong></p>
           </div>
         </Container>
 
