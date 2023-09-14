47ceb83e63cf38748da0221378ad122ba672935f
wliu080 <simonliu911@gmail.com>
2023-09-14 15:49:44 +0900

    dont need to specify width for StaticImageData

--- components/image-pagination/ImagePagination.tsx
+++ components/image-pagination/ImagePagination.tsx
@@ -1,91 +1,77 @@
-import React, { useState, useCallback } from 'react';
-import { RiArrowLeftSLine, RiArrowRightSLine } from 'react-icons/ri';
-import { Container, Row } from 'react-bootstrap';
-import Image from 'next/image';
+import React, { useState, useCallback } from "react"
+import { RiArrowLeftSLine, RiArrowRightSLine } from "react-icons/ri"
+import { Container } from "react-bootstrap"
+import Image, { StaticImageData } from "next/image"
 
 interface ImagePaginationProps {
   pages: {
-    src: string,
-    text: string,
-  }[],
-  barDisplay?: boolean,
+    src: StaticImageData
+    text: string
+  }[]
+  barDisplay?: boolean
 }
 
-const ImagePagination = ({
-  pages,
-  barDisplay = true,
-}: ImagePaginationProps) => {
-  const [activeIndex, setActiveIndex] = useState(0);
+const ImagePagination = ({ pages, barDisplay = true }: ImagePaginationProps) => {
+  const [activeIndex, setActiveIndex] = useState(0)
 
-  const onNextClick = useCallback((e: React.MouseEvent) => {
-    e.preventDefault();
-    if (activeIndex !== pages.length - 1) {
-      setActiveIndex(activeIndex => activeIndex + 1);
-    }
-  }, [pages, activeIndex, setActiveIndex]);
+  const onNextClick = useCallback(
+    (e: React.MouseEvent) => {
+      e.preventDefault()
+      if (activeIndex !== pages.length - 1) {
+        setActiveIndex((activeIndex) => activeIndex + 1)
+      }
+    },
+    [pages, activeIndex, setActiveIndex]
+  )
 
-  const onPrevClick = useCallback((e: React.MouseEvent) => {
-    e.preventDefault();
-    if (activeIndex !== 0) {
-      setActiveIndex(activeIndex => activeIndex - 1);
-    }
-  }, [activeIndex, setActiveIndex]);
+  const onPrevClick = useCallback(
+    (e: React.MouseEvent) => {
+      e.preventDefault()
+      if (activeIndex !== 0) {
+        setActiveIndex((activeIndex) => activeIndex - 1)
+      }
+    },
+    [activeIndex, setActiveIndex]
+  )
 
   const onMouseEnter = useCallback((e: React.MouseEvent) => {
-    e.preventDefault();
-  }, []);
+    e.preventDefault()
+  }, [])
   const onMouseLeave = useCallback((e: React.MouseEvent) => {
-    e.preventDefault();
-  }, []);
+    e.preventDefault()
+  }, [])
 
   return (
     <>
-      <Container className='d-flex justify-content-center'>
-        <button
-          onClick={onPrevClick}
-          className='image-pagination-button'
-        >
+      <Container className="d-flex justify-content-center">
+        <button onClick={onPrevClick} className="image-pagination-button">
           <RiArrowLeftSLine size={32} />
         </button>
-        <Container
-          className='image-pagination-images'
-          onMouseEnter={onMouseEnter}
-          onMouseLeave={onMouseLeave}
-        >
-          {
-            pages.map((img, idx) => (
-              <div
-                style={activeIndex === idx ? {display: 'block'} : {display: 'none'}}
-                key={`${img.src}_${idx}`}
-              >
-                <Image src={img.src} alt={img.text}/> 
-                {img.text && <span>{img.text}</span>}
-              </div>
-            ))
-          }
-
+        <Container className="image-pagination-images" onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
+          {pages.map((img, idx) => (
+            <div style={activeIndex === idx ? { display: "block" } : { display: "none" }} key={`${img.src}_${idx}`}>
+              <Image src={img.src} alt={img.text} />
+              {img.text && <span>{img.text}</span>}
+            </div>
+          ))}
         </Container>
-        <button
-          type='button'
-          onClick={onNextClick}
-          className='image-pagination-button'
-        >
+        <button type="button" onClick={onNextClick} className="image-pagination-button">
           <RiArrowRightSLine size={32} />
         </button>
       </Container>
-      {
-        barDisplay && <div className='d-flex text-center justify-content-center'>
+      {barDisplay && (
+        <div className="d-flex text-center justify-content-center">
           {pages.map((img, idx) => (
             <div
               key={`${img.src}_${idx}`}
-              className='bar-display'
-              style={activeIndex === idx ? {backgroundColor: '#285ABD'} : { backgroundColor: '#D6DDE6'}}
+              className="bar-display"
+              style={activeIndex === idx ? { backgroundColor: "#285ABD" } : { backgroundColor: "#D6DDE6" }}
             />
           ))}
         </div>
-      }
+      )}
     </>
-  );
-};
+  )
+}
 
-export default ImagePagination;
+export default ImagePagination
