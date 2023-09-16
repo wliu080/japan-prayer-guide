cbdda0e5c4cc19f3b94c45d34a88fa2933925da9
Thomas Bak <tombak98@gmail.com>
2023-09-15 21:19:54 -0400

    whoops, fixes for order section

--- components/common/OrderBook.tsx
+++ components/common/OrderBook.tsx
@@ -28,17 +28,17 @@ export default function OrderBook({
 
   function ENSection() {
     return (
-      <Container className="home-order-section bg-grey-2 d-flex flex-column align-items-center px-4 mt-4">
-        <Image alt="order-icon" src="/photos/home/hp_order_en.png" className="d-block d-md-none mt-3" />
+      <Container className={"home-order-section bg-grey-2 d-flex flex-column align-items-center px-4" + (language === "en" ? "" : " mt-4")}>
+        {/* <Image alt="order-icon" src="/photos/home/hp_order_en.png" className="d-block d-md-none mt-3" /> */}
         <div className="position-relative w-100 d-flex align-items-center flex-column">
-          <h1 className="w-auto bg-grey-2 p-3 text-grey-7 mt-md-3 mb-1 position-relative">{title}</h1>
+          <h1 className="w-auto bg-grey-2 p-3 text-grey-7 mt-3 mb-1 position-relative">{title}</h1>
           <div className="w-100 bg-grey-7 horizontal-bar position-relative"></div>
         </div>
-        <h2 className="text-primary fs-5 fw-bold mb-2">{blurb}</h2>
+        <h2 className="text-primary fs-4 fw-bold mb-2 mt-1">{blurb}</h2>
         <div className="d-flex flex-column flex-md-row align-items-center gap-3 mb-2">
           {orderRegionsMap.map((region) => (
             <Link
-              className="fs-6 bg-secondary-5 text-white text-center region text-decoration-none"
+              className="fs-5 fw-bold bg-secondary-5 text-white text-center region text-decoration-none"
               href={region.url}
               key={region.text}
             >
@@ -47,7 +47,7 @@ export default function OrderBook({
           ))}
         </div>
         <Link
-          className="fs-5 text-secondary-5 fw-bold text-decoration-underline mb-4"
+          className="fs-4 text-secondary-5 fw-bold text-decoration-underline mb-4"
           href="https://www.amazon.com/dp/B099KSSY79"
         >
           {orderPrompt}
@@ -58,24 +58,24 @@ export default function OrderBook({
 
   function JASection() {
     return (
-      <Container className="home-order-section bg-grey-2 d-flex flex-column align-items-center mt-4">
-        <Image alt="order-icon" src="/photos/home/hp_order_ja.png" className="d-block d-md-none mt-3" />
+      <Container className={"home-order-section bg-grey-2 d-flex flex-column align-items-center" + (language !== "en" ? "" : " mt-4")}>
+        {/* <Image alt="order-icon" src="/photos/home/hp_order_ja.png" className="d-block d-md-none mt-3" /> */}
         <div className="position-relative w-100 d-flex align-items-center flex-column">
           <h1 className="w-auto bg-grey-2 p-3 text-grey-7 mt-md-3 mb-1 position-relative">{orderJapan}</h1>
           <div className="w-100 bg-grey-7 horizontal-bar position-relative"></div>
         </div>
-        <h2 className="text-black fs-6 fst-italic mb-2 d-flex align-items-center gap-1">
+        <Link
+          className="fs-5 japan-order bg-grey-2 text-center text-secondary-5 border-secondary-5 fw-bold fs-5 mb-1 p-2 text-decoration-none border rounded"
+          href="https://docs.google.com/forms/d/e/1FAIpQLSf03r2GXDfFa17f5ICL_HTy_NuQOpaJcmNgRyFQN10ghgEYqQ/viewform"
+        >
+          {orderBooklet}
+        </Link>
+        <h2 className="text-black fs-5 fst-italic mb-2 d-flex align-items-center gap-1 mb-4">
           <IconContext.Provider value={{ size: "16px" }}>
             <RiInformationLine />
           </IconContext.Provider>
           {orderWarning}
         </h2>
-        <Link
-          className="bg-grey-2 text-secondary-5 border-secondary-5 fw-bold fs-5 mb-4 p-2 text-decoration-none border rounded"
-          href="https://docs.google.com/forms/d/e/1FAIpQLSf03r2GXDfFa17f5ICL_HTy_NuQOpaJcmNgRyFQN10ghgEYqQ/viewform"
-        >
-          {orderBooklet}
-        </Link>
       </Container>
     )
   }
