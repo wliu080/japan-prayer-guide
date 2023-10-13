ad7fb6eeed628d91979cd94bfb3fb6784b54e9b8
wliu080 <simonliu911@gmail.com>
2023-10-13 09:22:18 +0900

    Added snapshot tests

--- test/components/Topic/TopicDownloadables.test.tsx
+++ test/components/Topic/TopicDownloadables.test.tsx
@@ -2,6 +2,25 @@ import { render, screen } from "@testing-library/react"
 import TopicDownloadables from "../../../components/topic/TopicDownloadables"
 
 describe("Topic Downloadables", () => {
+    test("Snapshot test", () => {
+        // This test is to check against unintended changes.
+        // If the change is intentional you can update the snapshot with `jest --updateSnapshot`
+
+        const testTextArray = ["ab", "cd", "ef"]
+        const testTextArray2 = ["/", "/", "/", "/", "/"]
+        const testTitle = "Downloadables"
+        const testHeaders = ["hi", "test"]
+        const component = render(
+            <TopicDownloadables
+                labels={testTextArray}
+                links={testTextArray2}
+                title={testTitle}
+                headers={testHeaders}
+            />,
+        )
+        expect(component).toMatchSnapshot()
+    })
+
     test("Renders a section with the right text inside", () => {
         const testTextArray = ["Hey how's it going", "I'm doing fine", "Thanks", "great"]
         const testTextArray2 = ["/", "/", "/", "/", "/"]
