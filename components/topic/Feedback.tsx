558aefdc8271577c886efa447de81fcc9ede931c
wliu080 <simonliu911@gmail.com>
2024-01-16 17:42:28 +0900

    first pass of restructure

--- components/topic/Feedback.tsx
+++ components/topic/Feedback.tsx
@@ -1,12 +1,9 @@
-import { TFunction, Trans } from "next-i18next"
+import { Trans, useTranslation } from "next-i18next"
 import React from "react"
 import Button from "react-bootstrap/Button"
 
-interface feedbackProps {
-    topicTrans: TFunction
-}
-
-const Feedback = ({ topicTrans }: feedbackProps) => {
+const Feedback = () => {
+    const { t } = useTranslation("common")
     return (
         <section
             data-testid={"feedback-sect"}
@@ -14,11 +11,11 @@ const Feedback = ({ topicTrans }: feedbackProps) => {
             style={{ backgroundColor: "#EAF4FF" }}
         >
             <h1 data-testid={"feedback-title"} className="text-primary fs-2">
-                <Trans t={topicTrans} i18nKey="feedback.title" />
+                <Trans t={t} i18nKey="feedback.title" />
             </h1>
             <Button className="bg-secondary text-white border-0 p-2 w-100 mt-4" style={{ maxWidth: "300px" }}>
                 <p data-testid={"feedback-button"} className="m-0">
-                    <Trans t={topicTrans} i18nKey="feedback.button" />
+                    <Trans t={t} i18nKey="feedback.button" />
                 </p>
             </Button>
         </section>
--- pages/topics/[topicPage].tsx
+++ pages/topics/[topicPage].tsx
@@ -9,11 +9,11 @@ import Footer from "../../components/footer"
 import { useTranslation } from "next-i18next"
 import { serverSideTranslations } from "next-i18next/serverSideTranslations"
 import Feedback from "../../components/topic/Feedback"
-import ImageGroup from "../../components/topic/ImageGroup"
 import { TopicNav } from "../../components/topic/TopicNav"
 import React from "react"
 import NestedModal from "../../components/NestedModal/NestedModal"
 import PrayerPoints, { PrayerDisplayStyle } from "../../components/common/PrayerPoints"
+import CollapseBlock from "../../components/topic/CollapseBlock"
 
 export const getStaticPaths: GetStaticPaths = async () => {
     const paths = getTopicPageIds()
@@ -37,24 +37,26 @@ export const getStaticProps: GetStaticProps = async ({ params, locale }: any) =>
     return {
         props: {
             localeRef,
-            ...(await serverSideTranslations(locale, ["common", localeRef])),
+            ...(await serverSideTranslations(locale, ["common", "topic-pages", localeRef])),
         },
     }
 }
 
 export default function TopicPage({ localeRef }: { localeRef: string }) {
     const { t } = useTranslation(localeRef)
-    const { t: common } = useTranslation("common")
+    const { t: topicCommon } = useTranslation("topic-pages")
 
     // Objects holding translations
-    const bodyContent1: string[] = t("mainBody.content1", { returnObjects: true })
-    const bodyContent2: string[] = t("mainBody.content2", { returnObjects: true })
-    const bodyContent3: string[] = t("mainBody.content3", { returnObjects: true })
+    let textContent: string[] = t("textBody", { returnObjects: true })
+    textContent = Array.isArray(textContent) ? textContent : []
 
     const quoteContent: string = t("quote.content")
     const quoteSource: string = t("quote.source")
 
     const pageTitle: string = t("title")
+
+    const galleryLabel: string = topicCommon("galleryLabel")
+    const factsLabel: string = topicCommon("factsLabel")
 
     // States
     const [selected, setSelected] = React.useState<string>("About")
@@ -83,63 +85,45 @@ export default function TopicPage({ localeRef }: { localeRef: string }) {
                 {/* Topic Nav Component */}
                 <TopicNav selected={selected} setSelected={setSelected} topicTrans={t} />
 
-                <NestedModal />
-
-                {/* Video/Reel Placeholder */}
-                <Container className="py-5" id="topic-about">
-                    <div id="placeholder-image" className="w-100">
-                        Video Placeholder
-                    </div>
-                </Container>
-
-                {/* Placeholder text */}
-                <Container className="py-5">
-                    {bodyContent1.map((text: string, idx: number) => (
-                        <p key={idx + text}>{text}</p>
-                    ))}
-                </Container>
-
-                {/* Image Grid */}
-                <ImageGroup />
-
-                {/* Placeholder text */}
-                <Container className="py-5">
-                    {bodyContent2.map((text: string, idx: number) => (
-                        <p key={idx + text}>{text}</p>
-                    ))}
-                </Container>
-
-                {/* Infographics Placeholder */}
-                <Container>
-                    <div id="placeholder-image" className="w-100">
-                        Infographics Placeholder
-                    </div>
-                </Container>
-
                 {/* Placeholder for a quote */}
                 <Container className="d-flex flex-column justify-content-center align-items-center py-4 my-4">
                     <h1 className="text-secondary fs-1 text-center">{quoteContent}</h1>
                     <h2 className="text-secondary fs-6 text-center">{quoteSource}</h2>
                 </Container>
 
-                {/* Placeholder Image */}
-                <div id="placeholder-image" className="w-100 my-5">
-                    Placeholder
-                </div>
-
                 {/* Placeholder text */}
                 <Container className="py-5">
-                    {bodyContent3.map((text: string, idx: number) => (
+                    {textContent.map((text: string, idx: number) => (
                         <p key={idx + text}>{text}</p>
                     ))}
                 </Container>
+
+                {/* Images */}
+                <CollapseBlock title={galleryLabel}>
+                    <NestedModal />
+                    {/* Image Grid
+                        <ImageGroup /> */}
+                </CollapseBlock>
+
+                {/* Infographics Placeholder */}
+                <CollapseBlock title={factsLabel}>
+                    <Container>
+                        <div id="placeholder-image" className="w-100">
+                            Infographics Placeholder
+                        </div>
+                        {/* Placeholder Image */}
+                        <div id="placeholder-image" className="w-100 my-5">
+                            Placeholder
+                        </div>
+                    </Container>
+                </CollapseBlock>
 
                 {/* Prayer Points */}
                 <PrayerPoints topicTrans={t} displayStyle={PrayerDisplayStyle.TopicBottom} />
                 <br />
 
                 {/* Give us Feedback */}
-                <Feedback topicTrans={common} />
+                <Feedback />
 
                 {/* Downloads and Related */}
                 <TopicDownloadables topicTrans={t} />
