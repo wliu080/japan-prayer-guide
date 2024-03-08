c7c2ad5af03a7e6357e0738fd394f687230e9031
Tommy Jung <jung4351@gmail.com>
2024-03-07 20:38:11 -0800

    Add proxima nova as a font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { AppProps } from "next/app"
 import { appWithTranslation } from "next-i18next"
 import { Analytics } from "@vercel/analytics/react"
 import "../styles/main.scss"
+import "../public/fonts/proxima-nova-otf/stylesheet.css"
 
 function App({ Component, pageProps }: AppProps) {
     return (
