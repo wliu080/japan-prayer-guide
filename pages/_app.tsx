5445649f9161f2b1cfa2c580070c518ce6d9f101
Tommy Jung <jung4351@gmail.com>
2024-03-10 17:52:31 -0700

    Remove proxima nova

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import { AppProps } from "next/app"
 import { appWithTranslation } from "next-i18next"
 import { Analytics } from "@vercel/analytics/react"
 import "../styles/main.scss"
-import "../public/fonts/proxima-nova-otf/stylesheet.css"
 
 function App({ Component, pageProps }: AppProps) {
     return (
